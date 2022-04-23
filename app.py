8b173a39a153777f4dac7d0e546478d9953d5c75
magolalo <104237922+magolalo@users.noreply.github.com>
2022-04-23 20:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not os.path.isfile('mini.db'):
 #URL del proyecto
 @app.route("/", methods=["GET"])
 def index():
-    inicio = ('<html><h2>Click <a href="https://github.com/magolalo/DesafioMeLi">aquí</a> para comenzar.</h2></html>')
+    inicio = ('<h1>Click <a href="https://github.com/magolalo/DesafioMeLi">aquí</a> para comenzar.</h1>')
     return inicio
 
 #Ruta 1: Recibe el término que se desea cosultar como parte de la URL en <palabra> y
